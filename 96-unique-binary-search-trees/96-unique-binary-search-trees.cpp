c70ff7cb863e83434c56e0c9ea96aeb4f8250a25
salonisingh-23 <86800963+salonisingh-23@users.noreply.github.com>
2022-04-12 19:30:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (66.37%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -3,10 +3,10 @@ public:
     int numTrees(int n) {
         int catalan[n+1];
         catalan[0]=catalan[1]=1;
-        for(int i=1;i<=n;++i)
+        for(int i=1;i<=n;i++)
         {
             catalan[i]=0;
-            for(int j=0;j<i;++j)
+            for(int j=0;j<i;j++)
             {
                 catalan[i]+=catalan[j]*catalan[i-j-1];
             }
