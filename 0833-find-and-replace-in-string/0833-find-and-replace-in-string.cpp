6be40d436d75aab43b7e6cbcdc46eae0f1e08c48
salonisingh-23 <86800963+salonisingh-23@users.noreply.github.com>
2022-10-18 10:17:05 +0530

    Time: 23 ms (5.92%), Space: 13.7 MB (19.38%) - LeetHub

---
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
+        vector<pair<int, int>> indicesInDescendingOrder;
+        for(int i = 0; i < indices.size(); i++){
+            indicesInDescendingOrder.push_back({indices[i], i});
+        }
+        
+        sort(indicesInDescendingOrder.rbegin(), indicesInDescendingOrder.rend());
+        
+        for(int i = 0; i < indicesInDescendingOrder.size(); i++){
+            int indicesIndex = indicesInDescendingOrder[i].first;
+            int sourcesIndex = indicesInDescendingOrder[i].second;
+            
+            string sourcesStr = sources[sourcesIndex];
+            if(s.substr(indicesIndex, sourcesStr.size()) == sourcesStr){
+                s = s.substr(0, indicesIndex) + targets[sourcesIndex] + s.substr(indicesIndex + sourcesStr.size());
+            }
+        }
+        
+        return s;
+    }
+};
